cd6c6438567f15fbe597c35eb40f2dca06cb43b6
Kangsaga <saga1897@naver.com>
2024-01-31 17:05:28 +0900

    재 빌드

--- libs/addres.js
+++ libs/addres.js
@@ -1,3 +1,5 @@
+import { addressManager } from "@/utils/adressManager";
+
 export const getServerSideProps = async ({ req }) => {
   // 1-2에서 만들어준 클라이언트 요청 req를 이용해 ip주소를 찾는 매서드
   const ip = addressManager.getAddressFromServer(req);
