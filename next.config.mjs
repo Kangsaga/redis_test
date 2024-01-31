b1e2dc819d8ea5bc430390078eb26cca707460fc
Kangsaga <saga1897@naver.com>
2024-01-31 14:25:33 +0900

    dsadsadsa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "build",
+  output: "export",
+  exportTrailingSlash: "true",
 };
 
 export default nextConfig;
