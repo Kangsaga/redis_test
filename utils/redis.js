0c52dba74d387145f61b8cf0578cde56b9b156bb
Kangsaga <saga1897@naver.com>
2024-02-05 16:26:42 +0900

    .

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 
 export const redisConnect = new Redis({
-  host: "110.14.90.134",
-  port: 6379,
-  password: "",
+  host: "redis-11636.c322.us-east-1-2.ec2.cloud.redislabs.com",
+  port: 11636,
+  password: "AcThEsWbfUsKyOMVVM6Wbe0BZQBqpnkk",
 });
